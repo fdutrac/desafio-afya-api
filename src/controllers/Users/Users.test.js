f617f125cbb0278d7b1bf3bb6a3105b6675196f1
fdutrac <fabiohenrique@gestaointeligentesofware.com.br>
2021-06-08 22:00:48 -0300

    Corrige teste de usuário

--- src/controllers/Users/Users.test.js
+++ src/controllers/Users/Users.test.js
@@ -2,41 +2,72 @@
 const request = require('supertest');
 const app = require('../../../app');
 
+const testUser = {};
+
 const fakeInvalidUser = {
   name: 'ze',
   login: '12312312312323124125125125',
   password: '123',
 };
 
-const fakeInvalidId = 0;
+const fakeValidUser = {
+  name: 'José Matheus da silva',
+  login: 'josematheus12',
+  password: '123456',
+};
 
-// const fakeValidUser = {
-//   name: 'José Matheus da Silva',
-//   login: 'josematheus12',
-//   password: '123456',
-// };
+const fakeValidUpdate = {
+  name: 'José Matheus da Silva',
+  login: 'josematheus13',
+  password: '123321',
+};
+
+// Testa listar usuario
 
 it('should return status 200', async () => {
   const res = await request(app).get('/usuarios');
   expect(res.status).toBe(200);
 });
 
+// Testa cadastrar usuario inválido
+
 it('should return status 400', async () => {
-  const res = await request(app).post('/usuarios', fakeInvalidUser);
+  const res = await request(app).post('/usuarios').send(fakeInvalidUser);
   expect(res.status).toBe(400);
 });
 
+// Testa cadastrar usuario válido
+
+it('should return status 200', async () => {
+  const res = await request(app).post('/usuarios').send(fakeValidUser);
+  expect(res.status).toBe(200);
+  this.testUser = res.body;
+});
+
+// Testa alterar Usuário com dados inválidos
+
 it('should return status 400', async () => {
-  const res = await request(app).put('/usuarios/5', fakeInvalidUser);
+  const res = await request(app).put(`/usuarios/${this.testUser.id}`).send(fakeInvalidUser);
   expect(res.status).toBe(400);
 });
 
-// it('should return status 200', async () => {
-//   const res = await request(app).post('/usuarios', fakeValidUser);
-//   expect(res.status).toBe(200);
-// });
+// Testa alterar Usuário com dados válidos
+
+it('should return status 200', async () => {
+  const res = await request(app).put(`/usuarios/${this.testUser.id}`).send(fakeValidUpdate);
+  expect(res.status).toBe(200);
+});
+
+// Testa deletar usuário inexistente
 
 it('should return status 404', async () => {
-  const res = await request(app).delete(`/usuarios/${fakeInvalidId}`);
+  const res = await request(app).delete(`/usuarios/${(this.testUser + 1)}`);
   expect(res.status).toBe(404);
+});
+
+// Testa deletar usuário existente
+
+it('should return status 200', async () => {
+  const res = await request(app).delete(`/usuarios/${this.testUser.id}`);
+  expect(res.status).toBe(200);
 });
